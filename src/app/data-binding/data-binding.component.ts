afaf26a4f9a1a403c99628492874cc49bcae0e4b
MauricioGiacomello <giacomerro@gmail.com>
2021-04-13 17:05:44 -0300

    Iniciando o curso

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  numero: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  obterNumero(numero: any) {
+
+    this.numero = numero.value
+  }
+}
